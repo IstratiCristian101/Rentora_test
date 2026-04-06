12e1181a7475eddc5f1e1db185554dc8f6b44426
Gheorghe <cormangh2@gmail.com>
2026-04-06 21:32:49 +0300

    feat: implement ReviewActions business layer ( Core + Structure )

--- backend/Rentora.BusinessLayer/BusinessLogic.cs
+++ backend/Rentora.BusinessLayer/BusinessLogic.cs
@@ -10,4 +10,5 @@ public class BusinessLogic
     public IUserAction UserAction() => new UserActionExecution();
     public ISupportRequestAction SupportRequestAction() => new SupportRequestExecution();
     public IApartmentAction ApartmentAction() => new ApartmentActionExecution();
+    public IReviewAction ReviewAction() => new ReviewActionExecution();
 }
